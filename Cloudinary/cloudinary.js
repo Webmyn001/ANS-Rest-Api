32763d86923f898bf8fbb11f05cfdd9c0adc418d
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-03-22 15:25:13 -0700

    first

--- Cloudinary/cloudinary.js
+++ Cloudinary/cloudinary.js
@@ -3,8 +3,8 @@ const cloudinary = require("cloudinary").v2
 cloudinary.config({
     cloud_name: "dwsjspwnt",
     api_key: "398899769288939",
-    api_secret: "baec2HpxDpF1AwwMHYlxlFl7xjc"
-    secure:true
+    api_secret: "baec2HpxDpF1AwwMHYlxlFl7xjc",
+    secure: true
   });
   
 
